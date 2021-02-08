0037ceae94275f15bb18b8810fd2747a999b6ec1
Shreyas-hacker <75136935+Shreyas-hacker@users.noreply.github.com>
2021-02-08 12:32:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.secret_key = "Shreyasbigboi"
 
-app.config["MYSQL_HOST"] = 'localhost'
+app.config["MYSQL_HOST"] = '127.0.0.1'
 app.config["MYSQL_USER"] = 'root'
 app.config["MYSQL_PASSWORD"] = ''
 app.config["MYSQL_DB"] = 'bank account'
